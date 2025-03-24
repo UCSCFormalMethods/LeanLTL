96e4458c31df1fdb8f1abb1cbce7137ad7f9c037
Kyle Miller <kmill31415@gmail.com>
2025-03-24 12:35:25 -0700

    remove infinite trace assumption from an Inductino example

--- LeanLTL/Examples/Induction.lean
+++ LeanLTL/Examples/Induction.lean
@@ -19,3 +19,35 @@ example {σ : Type} (p : σ → ℕ) :
     specialize hn m
     use 1 + m
     omega
+
+example {σ : Type} (p : σ → ℕ) :
+    let n := TraceFun.of p;
+    ⊨ LLTL[G ((← n) < (← X n)) → ∀ m, F (m < (←ʷ n))] := by
+  simp +contextual [push_ltl]
+  intro t ht n
+  induction n with
+  | zero =>
+    use 1
+    simp
+    obtain ⟨m, ht⟩ := ht 0 (by simp)
+    simp at ht
+    simp [TraceFun.shift] at ht
+    obtain ⟨⟨h, rfl⟩, h'⟩ := ht
+    exists h
+    omega
+  | succ x ih =>
+    obtain ⟨m, hm, ih⟩ := ih
+    obtain ⟨k, ht, ht'⟩ := ht m hm
+    simp [TraceFun.shift] at ht
+    obtain ⟨hm', rfl⟩ := ht
+    use 1 + m
+    constructor
+    · omega
+    · have : m < t.length := by
+        generalize_proofs at ih
+        assumption
+      clear ht'
+      revert hm'
+      cases t.length <;> simp
+      norm_cast
+      omega
